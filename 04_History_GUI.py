a7d842746ae3e947a88a97541f80f9035fd54d7e
amaya26 <165841780+amaya26@users.noreply.github.com>
2025-02-20 11:57:58 +1300

    Add files via upload

---
+++ 04_History_GUI.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from functools import partial # To prevent unwanted windows
+
+
+class Converter:
+    """
+    Temperature conversion tool (C to F or F to C)
+    """
+
+    def __init__(self):
+
+
+    def to_history(self):
+
+
+class His
